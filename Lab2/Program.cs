6a881d1259f05f3e9bb3d10b73d58604627e8683
Arseniy <molodeckij.b.2017@fml31.ru>
2018-02-09 22:13:42 +0300

    Created 2 lab

---
+++ Lab2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RootDictionary dictionary = new RootDictionary();
+            dictionary.Run();
+        }
+    }
+}
